ed66905e46980ff8a6672a70ce0729eb7bd987af
rlorcac <raimundo.lorca.c@ug.uchile.cl>
2022-12-15 15:50:22 -0300

    Portado inverse_kinematics_node.py

--- ros2_ws/src/inverse_kinematics/inverse_kinematics/inverse_kinematics_node.py
+++ ros2_ws/src/inverse_kinematics/inverse_kinematics/inverse_kinematics_node.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-import rospy
+import rclpy
+from rclpy.node import Node
 from duckietown_msgs.msg import WheelsCmdStamped, Twist2DStamped
-from duckietown_msgs.srv import SetValueRequest, SetValueResponse, SetValue
-from std_srvs.srv import EmptyRequest, EmptyResponse, Empty
 from numpy import *
 import rospkg
 import yaml
@@ -16,139 +15,53 @@ import os.path
 # Copiado de https://github.com/Duckietown-Chile/Software/blob/master/catkin_ws/src/dagu_car/script/inverse_kinematics_node.py
 # Adaptado a ROS2+rclpy en 2022-12 por Raimundo Lorca Correa <raimundo.lorca.c@ug.uchile.cl>
 
-class InverseKinematicsNode(object):
-    def __init__(self):
+class InverseKinematicsNode(Node):
+    def __init__(self, node_name):
+        super().__init__(node_name)
         # Get node name and vehicle name
-        self.node_name = rospy.get_name()
-        self.veh_name = self.node_name.split("/")[1]        
-
-        # Set parameters using yaml file
-        self.readParamFromFile()
+        self.node_name = node_name
 
         # Set local variable by reading parameters
-        self.gain = self.setup_parameter("~gain", 1.0)
-        self.trim = self.setup_parameter("~trim", 0.0)
-        self.baseline = self.setup_parameter("~baseline", 0.1)
-        self.radius = self.setup_parameter("~radius", 0.0318)
-        self.k = self.setup_parameter("~k", 27.0)
-        self.limit = self.setup_parameter("~limit", 1.0)
+        self.declare_parameter("~/gain", 1.0)
+        self.declare_parameter("~/trim", 0.0)
+        self.declare_parameter("~/baseline", 0.1)
+        self.declare_parameter("~/radius", 0.0318)
+        self.declare_parameter("~/k", 27.0)
+        self.declare_parameter("~/limit", 1.0)
+        
         self.limit_max = 1.0
         self.limit_min = 0.0
-
-        # Prepare services
-        self.srv_set_gain = rospy.Service("~set_gain", SetValue, self.cbSrvSetGain)
-        self.srv_set_trim = rospy.Service("~set_trim", SetValue, self.cbSrvSetTrim)
-        self.srv_set_baseline = rospy.Service("~set_baseline", SetValue, self.cbSrvSetBaseline)
-        self.srv_set_radius = rospy.Service("~set_radius", SetValue, self.cbSrvSetRadius)
-        self.srv_set_k = rospy.Service("~set_k", SetValue, self.cbSrvSetK)
-        self.srv_set_limit = rospy.Service("~set_limit", SetValue, self.cbSrvSetLimit)
-        self.srv_save = rospy.Service("~save_calibration", Empty, self.cbSrvSaveCalibration)
+        
+        self.gain = self.get_parameter("~/gain").value
+        self.trim = self.get_parameter("~/trim").value
+        self.baseline = self.get_parameter("~/baseline").value
+        self.radius = self.get_parameter("~/radius").value
+        self.k = self.get_parameter("~/k").value
+        self.limit = self.get_parameter("~/limit").value
 
         # Setup the publisher and subscriber
-        self.sub_car_cmd = rospy.Subscriber("~car_cmd", Twist2DStamped, self.car_cmd_callback)
-        self.pub_wheels_cmd = rospy.Publisher("~wheels_cmd", WheelsCmdStamped, queue_size=1)
-        rospy.loginfo("[%s] Initialized.", self.node_name)
+        self.sub_car_cmd = self.create_subscription(Twist2DStamped, "~/car_cmd", self.car_cmd_callback, qos_profile=0)
+        self.pub_wheels_cmd = self.create_publisher(WheelsCmdStamped, "~/wheels_cmd", qos_profile=0)
+        self.get_logger().info("[%s] Initialized." %(self.node_name))
         self.printValues()
 
-    def readParamFromFile(self):
-        # Check file existence
-        fname = self.getFilePath(self.veh_name)
-        # Use default.yaml if file doesn't exsit
-        if not os.path.isfile(fname):
-            rospy.logwarn("[%s] %s does not exist. Using default.yaml." %(self.node_name,fname))
-            fname = self.getFilePath("default")
-
-        with open(fname, 'r') as in_file:
-            try:
-                yaml_dict = yaml.load(in_file)
-            except yaml.YAMLError as exc:
-                rospy.logfatal("[%s] YAML syntax error. File: %s fname. Exc: %s" %(self.node_name, fname, exc))
-                rospy.signal_shutdown()
-                return
-
-        # Set parameters using value in yaml file
-        if yaml_dict is None:
-            # Empty yaml file
-            return
-        for param_name in ["gain", "trim", "baseline", "k", "radius", "limit"]:
-            param_value = yaml_dict.get(param_name)
-            if param_name is not None:
-                rospy.set_param("~"+param_name, param_value)
-            else:
-                # Skip if not defined, use default value instead.
-                pass
-
-    def getFilePath(self, name):
-        rospack = rospkg.RosPack()
-        return rospack.get_path('duckietown')+'/config/baseline/calibration/kinematics/' + name + ".yaml"        
-
-    def saveCalibration(self):
-        # Write to yaml
-        data = {
-            "calibration_time": time.strftime("%Y-%m-%d-%H-%M-%S"),
-            "gain": self.gain,
-            "trim": self.trim,
-            "baseline": self.baseline,
-            "radius": self.radius,
-            "k": self.k,
-            "limit": self.limit,
-        }
-
-        # Write to file
-        file_name = self.getFilePath(self.veh_name)
-        with open(file_name, 'w') as outfile:
-            outfile.write(yaml.dump(data, default_flow_style=False))
-        # Printout
-        self.printValues()
-        rospy.loginfo("[%s] Saved to %s" %(self.node_name, file_name))
-
-    def cbSrvSaveCalibration(self, req):
-        self.saveCalibration()
-        return EmptyResponse()
-
-    def cbSrvSetGain(self, req):
-        self.gain = req.value
-        self.printValues()
-        return SetValueResponse()
-
-    def cbSrvSetTrim(self, req):
-        self.trim = req.value
-        self.printValues()
-        return SetValueResponse()
-
-    def cbSrvSetBaseline(self, req):
-        self.baseline = req.value
-        self.printValues()
-        return SetValueResponse()
-
-    def cbSrvSetRadius(self, req):
-        self.radius = req.value
-        self.printValues()
-        return SetValueResponse()
-
-    def cbSrvSetK(self, req):
-        self.k = req.value
-        self.printValues()
-        return SetValueResponse()
-
-    def cbSrvSetLimit(self, req):
-        self.limit = self.setLimit(req.value)
-        self.printValues()
-        return SetValueResponse()
+#    def getFilePath(self, name):
+#        rospack = rospkg.RosPack()
+#        return rospack.get_path('duckietown')+'/config/baseline/calibration/kinematics/' + name + ".yaml"        
 
     def setLimit(self, value):
         if value > self.limit_max:
-            rospy.logwarn("[%s] limit (%s) larger than max at %s" % (self.node_name, value, self.limit_max))
+            self.get_logger().warn("[%s] limit (%s) larger than max at %s" % (self.node_name, value, self.limit_max))
             limit = self.limit_max
         elif value < self.limit_min:
-            rospy.logwarn("[%s] limit (%s) smaller than allowable min at %s" % (self.node_name, value, self.limit_min))
+            self.get_logger().warn("[%s] limit (%s) smaller than allowable min at %s" % (self.node_name, value, self.limit_min))
             limit = self.limit_min
         else:
             limit = value
         return limit
 
     def printValues(self):
-        rospy.loginfo("[%s] gain: %s trim: %s baseline: %s radius: %s k: %s limit: %s" % (self.node_name, self.gain, self.trim, self.baseline, self.radius, self.k, self.limit))
+        self.get_logger().info("[%s] gain: %s trim: %s baseline: %s radius: %s k: %s limit: %s" % (self.node_name, self.gain, self.trim, self.baseline, self.radius, self.k, self.limit))
 
     def car_cmd_callback(self, msg_car_cmd):
         # assuming same motor constants k for both motors
@@ -183,10 +96,10 @@ class InverseKinematicsNode(object):
         value = rospy.get_param(param_name, default_value)
         # Write to parameter server for transparency
         rospy.set_param(param_name, value)
-        rospy.loginfo("[%s] %s = %s " % (self.node_name, param_name, value))
+        self.get_logger().info("[%s] %s = %s " % (self.node_name, param_name, value))
         return value
 
 if __name__ == '__main__':
-    rospy.init_node('inverse_kinematics_node', anonymous=False)
-    inverse_kinematics_node = InverseKinematicsNode()
-    rospy.spin()
+    rclpy.init()
+    inverse_kinematics_node = InverseKinematicsNode('inverse_kinematics_node')
+    rclpy.spin(inverse_kinematics_node)
