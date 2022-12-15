29d77453cf2e602a31159adbb66c009eed4d6eab
rlorcac <raimundo.lorca.c@ug.uchile.cl>
2022-12-15 11:38:07 -0300

    Modificar setup.py para duckiebot_driver y ros_cap

--- ros2_ws/src/duckiebot_driver/setup.py
+++ ros2_ws/src/duckiebot_driver/setup.py
@@ -1,3 +1,5 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'duckiebot_driver'
@@ -7,6 +9,7 @@ setup(
     version='0.0.0',
     packages=[package_name],
     data_files=[
+        (os.path.join('share', package_name), glob('launch/*.xml')),
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
